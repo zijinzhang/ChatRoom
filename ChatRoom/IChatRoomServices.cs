7ec3a9b3825c751d5e03ef37d4274bdde9eb1f53
yangbyron <yangbyron998@gmail.com>
2016-09-13 16:59:27 -0700

    add a plain WCF services

---
+++ ChatRoom/IChatRoomServices.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ChatRoom {
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IChatRoomServices" in both code and config file together.
+    [ServiceContract]
+    public interface IChatRoomServices {
+        [OperationContract]
+        void DoWork();
+    }
+}
