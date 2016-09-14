512191ab84335486e065ed79d727b5c056c2b2ea
inkin <inkin@DESKTOP-FKGI94F>
2016-09-14 11:19:03 -0700

    Get Name

---
+++ ChatRoomMain/ChatRoom.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using ChatRoom;
+
+namespace ChatRoomMain
+{
+    /// <summary>
+    /// Interaction logic for ChatRoom.xaml
+    /// </summary>
+    public partial class ChatRoom : Window
+    {
+        private string name;
+        public ChatRoom(string name)
+        {
+            InitializeComponent();
+            this.name = name;
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(name);
+        }
+
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
