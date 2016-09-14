51606df59f5d01d3f7aadb318f608d72f41e10be
zijin <zijinzhang0607@hotmail.com>
2016-09-13 18:02:48 -0700

    update interfaces, add login and logout

--- ChatRoom/ChatRoomServices.svc.cs
+++ ChatRoom/ChatRoomServices.svc.cs
@@ -8,8 +8,28 @@ using System.Text;
 namespace ChatRoom {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ChatRoomServices" in code, svc and config file together.
     // NOTE: In order to launch WCF Test Client for testing this service, please select ChatRoomServices.svc or ChatRoomServices.svc.cs at the Solution Explorer and start debugging.
-    public class ChatRoomServices:IChatRoomServices {
+    public class ChatRoomServices:IChatRoomSe1rvices {
+        public void Add(string message) {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(string message) {
+            throw new NotImplementedException();
+        }
+
         public void DoWork() {
+        }
+
+        public bool Login(string userName) {
+            throw new NotImplementedException();
+        }
+
+        public void Logout(string userName) {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<string> ShowMessage() {
+            throw new NotImplementedException();
         }
     }
 }
